faac38d424f827a4c44cd199482651c62d1c272e
Turese Anderson <turese@berkeley.edu>
2019-02-08 10:15:09 -0800

    untested code

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,13 +3,46 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
-  # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+
+
+  # Get a word from remote "random word" service
+  def initialize()
+    return initialize(self.get_random_word())
+  end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = 0
+    @word_with_guesses = '-' * @word.length
+    return self
+  end
+
+  def self.guess(letter)
+    if guesses.include? letter
+      return
+    end
+    if @word.include? letter 
+      word_with_guesses[@word.index(letter)] = letter
+    else 
+      wrong_guesses += letter
+    end
+    guesses += letter
+  end
+
+  def self.check_win_or_lose() 
+    if @word_with_guesses == @word 
+      return :win
+    elsif @wrong_guesses >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
