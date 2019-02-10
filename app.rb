02309c76ed31df66b011f17756f3a8581a57d6ab
Turese Anderson <turese@berkeley.edu>
2019-02-10 13:04:38 -0800

    cucumber tests

--- app.rb
+++ app.rb
@@ -40,8 +40,19 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter)
-    redirect '/show'
+    if @game.guess(letter)
+    wincondition = @game.check_win_or_lose()
+      if wincondition == :play
+        redirect '/show'
+      elsif wincondition == :lose
+        redirect '/lose'
+      else
+        redirect '/win'
+      end
+    else
+      flash[:message]= "You have already used that letter"
+      redirect '/show'
+    end
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
